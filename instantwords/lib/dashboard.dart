d48eaab24b8d5bb2767219e7db1a6dd72d8ada7b
Ines Oliveira e Silva <inesoliveiraesilva@gmail.com>
2020-11-20 09:38:22 +0000

    Fixed overflow bug

--- instantwords/lib/dashboard.dart
+++ instantwords/lib/dashboard.dart
@@ -6,7 +6,8 @@ class Dashboard extends StatefulWidget {
   final SpeechToTextProvider _speechProvider;
   final translator;
 
-  Dashboard(this._fireStore, this._storage, this._speechProvider,this.translator);
+  Dashboard(
+      this._fireStore, this._storage, this._speechProvider, this.translator);
 
   @override
   State<Dashboard> createState() => new _DashboardState();
@@ -17,8 +18,8 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBarWidget(
-          widget._fireStore, widget._storage, widget._speechProvider,widget.translator),
+      appBar: AppBarWidget(widget._fireStore, widget._storage,
+          widget._speechProvider, widget.translator),
       body: new Container(
         padding: EdgeInsets.all(16.0),
         child: new Column(
@@ -39,11 +40,13 @@ class _DashboardState extends State<Dashboard> {
   }
 
   Widget _buildConferenceBlocks() {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: _getConferences(),
+    return Expanded(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: _getConferences(),
+        ),
       ),
     );
   }
@@ -104,12 +107,17 @@ class _DashboardState extends State<Dashboard> {
   }
 
   void _conferencePressed(int index, String language) {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => SpectatorWidget(widget._fireStore,
-                widget._storage, widget._speechProvider,index, language,widget.translator),
-          ));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SpectatorWidget(
+              widget._fireStore,
+              widget._storage,
+              widget._speechProvider,
+              index,
+              language,
+              widget.translator),
+        ));
   }
 
   void _createConferencePressed() {
@@ -133,7 +141,7 @@ class CreateConferencePage extends StatefulWidget {
   final translator;
 
   CreateConferencePage(this._fireStore, this._storage, this._speechProvider,
-      this.conference_Size,this.translator);
+      this.conference_Size, this.translator);
 
   @override
   State<CreateConferencePage> createState() => new _CreateConferencePageState();
@@ -237,163 +245,17 @@ class _CreateConferencePageState extends State<CreateConferencePage> {
   }
 
   void _createConferencePressed() {
-    widget._storage.addConference(_name, _language,context.read<FireAuth>().currentUser.uid);
+    widget._storage.addConference(
+        _name, _language, context.read<FireAuth>().currentUser.uid);
     Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-            builder: (context) => ProviderDemoApp(widget._fireStore,
-                widget._storage, widget._speechProvider, widget.conference_Size, _language,widget.translator)
-    ));
+            builder: (context) => ProviderDemoApp(
+                widget._fireStore,
+                widget._storage,
+                widget._speechProvider,
+                widget.conference_Size,
+                _language,
+                widget.translator)));
   }
 }
-
-/*
-  @override
-  Widget build(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      body: Container(
-          padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
-          height: 220,
-          width: double.maxFinite,
-          child: Card(
-            elevation: 5,
-            child: Padding(
-              padding: EdgeInsets.all(7),
-              child: Stack(children: <Widget>[
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Stack(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 5),
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                cryptoIcon(),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                cryptoNameSymbol(),
-                                Spacer(),
-                                cryptoChange(),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                changeIcon(),
-                                SizedBox(
-                                  width: 20,
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: <Widget>[cryptoAmount()],
-                            )
-                          ],
-                        )
-                      )
-                    ],
-                  ),
-                )
-              ]),
-            ),
-          ),
-      )
-    )
-  );
- }
-
- Widget cryptoIcon() {
-   return Padding(
-     padding: const EdgeInsets.only(left: 15.0),
-     child: Align(
-         alignment: Alignment.centerLeft,
-         child: Icon(
-           Icons.favorite,
-           color: Colors.amber,
-           size: 40,
-         )),
-   );
- }
- Widget cryptoNameSymbol() {
-   return Align(
-     alignment: Alignment.centerLeft,
-     child: RichText(
-       text: TextSpan(
-         text: 'Bitcoin',
-         style: TextStyle(
-             fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
-         children: <TextSpan>[
-           TextSpan(
-               text: '\nBTC',
-               style: TextStyle(
-                   color: Colors.grey,
-                   fontSize: 15,
-                   fontWeight: FontWeight.bold)),
-         ],
-       ),
-     ),
-   );
-  }
-Widget cryptoChange() {
-  return Align(
-    alignment: Alignment.topRight,
-    child: RichText(
-      text: TextSpan(
-        text: '+3.67%',
-        style: TextStyle(
-            fontWeight: FontWeight.bold, color: Colors.green, fontSize: 20),
-        children: <TextSpan>[
-        TextSpan(
-            text: '\n+202.835',
-            style: TextStyle(
-                color: Colors.green,
-                fontSize: 15,
-                fontWeight: FontWeight.bold)),
-      ],
-    ),
-  ),
-);
-}
-Widget changeIcon() {
-  return Align(
-      alignment: Alignment.topRight,
-      child: Icon(
-        Icons.favorite,
-        color: Colors.green,
-        size: 30,
-      ));
-}
-Widget cryptoAmount() {
-  return Align(
-    alignment: Alignment.centerLeft,
-    child: Padding(
-      padding: const EdgeInsets.only(left: 20.0),
-      child: Row(
-        children: <Widget>[
-          RichText(
-            textAlign: TextAlign.left,
-            text: TextSpan(
-              text: '\n\$12.279',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 35,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: '\n0.1349',
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold)),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
- }
-}*/
